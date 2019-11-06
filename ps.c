15e4c801cdc2ce7a1ee2337aada38bfaa298657e
Ankith Ede <ankithe@gmail.com>
2019-11-05 23:42:25 -0500

    arkansas program

---
+++ ps.c
@@ -0,0 +1,10 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(void)
+{
+ printf(1, "Asanka's first user program on xv6\n");
+ exit();
+}
--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,9 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int
+sys_crsp(void)
+{
+    return 1975;
+}
